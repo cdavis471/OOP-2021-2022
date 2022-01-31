1e4e0480faf56a7363b9087f050d5ec6fd2c8ce6
C20441826 Conor Davis <c20441826@mytudublin.ie>
2022-01-31 13:00:24 +0000

    monday lecture - illuminati triangle in processing

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -11,57 +11,41 @@ public class HelloProcessing extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
+		//colorMode(HSB);
 		background(0);
-
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
-
-		float range = 5;
-
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
-
-		smooth();
-		
 	}
-
-	float x1, y1, x2, y2;
-	float x1dir, x2dir, y1dir, y2dir;
-	float c = 0;
 	
 	public void draw()
 	{	
-		strokeWeight(2);
-		stroke(c, 255, 255);
-		c = (c + 1f) % 255;
-		line(x1, y1, x2, y2);
+		/* basics of drawing in processing
+		stroke(255);
+		//background(200);//single parameter - grayscale
+		background(255,255,0);
+		fill(0,0,255);
+		//noFill();
+		//noStroke();
+		line(10,10,200,200);//x1, y1, x2, y2
+		circle(200,250, 50);//cx, cy, r
+		ellipse(50,200,90,200);//cx, cy, w, h
+		rectMode(CENTER);//first two param - center of rectangle
+		rect(300,20,100,20);//tlx, tly, w, h
+		rectMode(CORNER);//first two param - corner of rectangle
+		rect(200,20,100,20);//tlx, tly, w, h
+		point(20,300);
+		triangle(10, 400, 50, 450, 300, 200);//x1, y1, x2, y2, x3, y3
+		*/
 
-		x1 += x1dir;
-		x2 += x2dir;
-		y1 += y1dir;
-		y2 += y2dir;
-		
-		if (x1 < 0 || x1 > width)
-		{
-			x1dir = - x1dir;
-		}
-		if (y1 < 0 || y1 > height)
-		{
-			y1dir = - y1dir;
-		}
-
-		if (x2 < 0 || x2 > width)
-		{
-			x2dir = - x2dir;
-		}
-		if (y2 < 0 || y2 > height)
-		{
-			y2dir = - y2dir;
-		}
+		//exercise
+		colorMode(RGB);
+		noStroke();
+		background(255,0,0);
+		fill(255,255,0);
+		circle(250,250,400);
+		fill(0,255,255);
+		triangle(250,10,40,390,460,390);
+		fill(200,200,200);
+		ellipse(250,200,187,90);
+		fill(0,0,0);
+		circle(250,200,75);
 	}
 }
