4a6115d08938ad700132caca4c40f539e5a34bb3
C20441826 Conor Davis <c20441826@mytudublin.ie>
2022-03-25 20:42:49 +0000

    Lab 7 - Game of Life

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -6,22 +6,77 @@ public class Life extends PApplet {
 	
 	LifeBoard board;
 
+	//declare variables
+	int mode = 0;
+	int pause = 0;
 
-	public void settings() {
+	//key pressed
+	public void keyPressed() 
+	{
+		//check key pressed
+		if (key >= '0' && key <= '9') 
+		{
+			mode = key - '0';
+		}//end if statement
+		//spacebar pressed
+		if (keyCode == ' ') 
+		{
+			//pause if not paused
+            if(pause == 0)
+			{
+				pause = 1;
+			}//end if statement
+			//unpause if paused
+			else
+			{
+				pause = 0;
+			}//end else statement
+        }//end if statement
+		//1 pressed
+		if(keyCode == '1')
+		{
+			//randomise board
+			board.randomise();
+		}//end if statement
+		//2 pressed
+		if(keyCode == '2')
+		{
+			//kill everything to clear board
+			board.kill();
+		}//end if statement
+		//3 is pressed
+		if(keyCode == '3')
+		{
+			//draw cross right across and top / bottom
+			board.cross();
+		}//end if statement
+	}//end keyPressed method
+
+	//settings
+	public void settings() 
+	{
 		size(1000, 1000);
 		board = new LifeBoard(200, this);
 		board.randomise();
 		println(board.countCellsAround(1, 1));
+	}//end settings method
 
-	}
-
-	public void setup() {
+	//setup
+	public void setup() 
+	{
 		colorMode(RGB);
-
-	}
+	}//end setup method
 	
-	public void draw() {
+	//draw
+	public void draw() 
+	{
+		//render board
 		board.render();
-		board.update();
-	}
+		//check if paused
+		if(pause != 1)
+		{
+			board.update();
+		}//end if statement
+	}//end draw method
+
 }
--- java/src/ie/tudublin/LifeBoard.java
+++ java/src/ie/tudublin/LifeBoard.java
@@ -2,7 +2,10 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class LifeBoard {
+public class LifeBoard 
+{
+    
+    //variables
     boolean[][] board;
     boolean[][] next;
     int size;
@@ -16,68 +19,70 @@ public class LifeBoard {
         this.size = size;
         this.pa = pa;
         cellSize = pa.width / (float) size;
-    }
+    }//end LifeBoard
 
+    //randomise
     public void randomise()
     {
+        //iteration through rows
         for(int row = 0 ; row < size ; row ++)
         {
+            //iterate through column
             for(int col = 0 ; col < size ; col ++)
             {
+                //randomise
                 board[row][col] = pa.random(1.0f) > 0.5f;
-            }
-        }
-    }
+            }//end for loop
+        }//end for loop
+    }//end randomise method
 
+    //update board
     public void update()
     {
-        // If cell is alive
-        // 2 -3 - Survives
-        // if a dead cell has 3 neighbours - comes to life
-
+        //If cell is alive
+        //2 - 3 -> Survives
+        //if a dead cell has 3 neighbours - comes to life
         for(int row = 0 ; row < size ; row ++)
         {
             for (int col = 0 ; col < size ; col ++)
             {
+                //counter
                 int count = countCellsAround(row, col);
-
+                //check if alive at current row and column
                 if (isAlive(row, col))
                 {
                     if (count == 2 || count == 3)
                     {
                         next[row][col] = true;
-                    }
+                    }//end if statement
                     else
                     {
                         next[row][col] = false;
-                    }
-                }
+                    }//end else statement
+                }//end if statement
                 else
                 {
                     if (count == 3)
                     {
                         next[row][col] = true;
-                    }
+                    }//end if statement
                     else
                     {
                         next[row][col] = false;
-                    }
-                }
-            }
-        }
-
+                    }//end else statement
+                }//end else statement
+            }//end for loop
+        }//end for loop
         boolean[][] temp;
         temp = board;
         board = next;
         next = temp;
-    }
+    }//end update method
 
+    //count cells around
     public int countCellsAround(int row, int col)
     {
         int count = 0;
-
-        // Your bit goes here!
-
         for(int i = row - 1 ; i <= row + 1 ; i ++)
         {
             for(int j = col -1 ; j <= col + 1; j ++)
@@ -86,27 +91,41 @@ public class LifeBoard {
                 {
                     if (isAlive(i, j))
                     {
+                        //count this cell since alive
                         count ++;
-                    }
-                }
-            }
-        }
-
+                    }//end if statement
+                }//end if statement
+            }//end for loop
+        }//end for loop
         return count;
-    }
+    }//end countCellsAround
 
+    //is alive
     public boolean isAlive(int row, int col)
     {
+        //is alive to return location
         if (row >= 0 && row < size && col >= 0 && col < size)
         {
             return board[row][col]; 
-        }
+        }//end if statement
+        //not alive
         else
         {
             return false;
-        }
-    }
+        }//end else statement
+    }//end isAlive
 
+    //draw cell
+    public void drawCell(boolean[][] board, int row, int col)
+    {
+        if (row >= 0 && row < size && col >= 0 && col < size)
+        {
+            //set current location on board to true
+            board[row][col] = true;
+        }//end if statement
+    }//end drawCell
+
+    //render
     public void render()
     {
         pa.background(0);
@@ -114,22 +133,48 @@ public class LifeBoard {
         {
             for(int col = 0 ; col < size ; col ++)
             {
+                //calculate
                 float x = PApplet.map(col, 0, size, 0, pa.width);
                 float y = PApplet.map(row, 0, size, 0, pa.height);
                 x = cellSize * col;
                 y = cellSize * row;
-
                 if (board[row][col])
                 {
                     pa.fill(0, 255, 0);
-                }
+                }//end if statement
                 else
                 {
                     pa.noFill();
-                }
+                }//end else statement
                 pa.rect(x, y, cellSize, cellSize);
-            }
-        }
-        
-    }
-}
+            }//end for loop
+        }//end for loop
+    }//end render method
+
+    //kill
+    public void kill()
+    {
+        //iterate through rows
+        for(int row = 0 ; row < size ; row ++)
+        {
+            //iterate through columns
+            for(int col = 0 ; col < size ; col ++)
+            {
+                //set cell to false
+                board[row][col] = false;
+            }//end for loop
+        }//end for loop
+    }//end kill method
+
+    //cross
+    public void cross()
+    {
+        for(int i = 0;i < size;i++)
+        {
+            //draw lines top to bottom and left to right --> exactly through middle
+            drawCell(board, size / 2, i);
+            drawCell(board, i, size / 2);
+        }//end for loop
+    }//end cross method
+
+}//end LifeBoard class
